a4b5139c25f2367a09adbaa2b076b16ecdf291e0
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-26 00:08:01 -0700

    Add vector of node pointers neighbors

--- src/route_model.h
+++ src/route_model.h
@@ -17,6 +17,7 @@ class RouteModel : public Model {
         float h_value = std::numeric_limits<float>::max();
         float g_value = 0.0;
         bool visited = false;
+        std::vector<Node *> neighbors;
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
       
