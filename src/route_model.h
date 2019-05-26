19aa04ecfb44575f299abffbe2ae5a93a0cdc3f5
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-25 23:59:01 -0700

    Add h_value and initialize to maximum float value.

--- src/route_model.h
+++ src/route_model.h
@@ -14,6 +14,7 @@ class RouteModel : public Model {
       public:
         // Add public Node variables and methods here.
         Node *parent = nullptr;
+        float h_value = std::numeric_limits<float>::max();
         Node(){}
         Node(int idx, RouteModel * search_model, Model::Node node) : Model::Node(node), parent_model(search_model), index(idx) {}
       
