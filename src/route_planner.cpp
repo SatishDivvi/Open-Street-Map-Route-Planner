a2f12117e0fc7a953b372e79fcc2af20be9c0998
Divvi Naga Venkata Satish <divvinagavenkata.satish69@gmail.com>
2019-05-28 20:01:27 -0700

    Define ConstructFinalPath Function

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -10,3 +10,7 @@ RoutePlanner::RoutePlanner(RouteModel &model, float start_x, float start_y, floa
     start_node = &m_Model.FindClosestNode(start_x, start_y);
     end_node = &m_Model.FindClosestNode(end_x, end_y);
 }
+
+std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node *current_node) {
+    
+}
